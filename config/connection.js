da0148aacf23892168a7c0109ae20d48da45a3d9
Andrew.K <akirillychev@yahoo.com>
2020-11-02 20:16:54 -0500

    updated heroku connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,21 @@
+var connection=null;
 // Set up MySQL connection.
 var mysql = require("mysql");
-
-var connection = mysql.createConnection({
-  host: "127.0.0.1",
-  port: 3306,
-  user: "root",
-  password: "Hardwater55",
-  database: "burger_db"
-});
+if (process.env.JAWSDB_URL && process.env.JAWSDB_URL.length>0)
+{
+  connection = mysql.createConnection(
+    proces.env.JAWSDB_URL
+  );
+} else {
+  connection = mysql.createConnection({
+    host: "127.0.0.1",
+    port: 3306,
+    user: "root",
+    password: "Hardwater55",
+    database: "burger_db"
+  });
+}
+ 
 
 // Make connection.
 connection.connect(function(err) {
